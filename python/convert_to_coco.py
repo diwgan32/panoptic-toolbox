1ab44271b14b7222150a6ab6bf9b19da8071fea9
Diwakar Ganesan <diwakar.ganesan@gmail.com>
2021-10-10 11:21:28 -0700

    intermediate

--- python/convert_to_coco.py
+++ python/convert_to_coco.py
@@ -11,20 +11,54 @@ parser.add_argument('--dataset_path', type=str, default='/home/ubuntu/RawDataset
 
 args = parser.parse_args()
 
+def get_camera_info(calibration_file):
+    # Load camera calibration parameters
+    with open(calibration_file) as cfile:
+        calib = json.load(cfile)
+
+    # Cameras are identified by a tuple of (panel#,node#)
+    cameras = {(cam['panel'],cam['node']):cam for cam in calib['cameras']}
+
+    # Convert data into numpy arrays for convenience
+    for k,cam in cameras.iteritems():    
+        cam['K'] = np.matrix(cam['K'])
+        cam['distCoef'] = np.array(cam['distCoef'])
+        cam['R'] = np.matrix(cam['R'])
+        cam['t'] = np.array(cam['t']).reshape((3,1))
+
+    return cameras
+
 
 def process_helper(sequence):
     seq_name = sequence
     data_path = args.dataset_path
     hd_skel_json_path = data_path+seq_name+'/hdPose3d_stage1_coco19/'
+    cameras = get_camera_info(os.path.join(data_path, seq_name, f"calibration_{seq_name}.json"))
     for i in range(10):
         hd_vid_name = f"hd_00_{i:02d}.mp4"
         hd_vid_path = os.path.join(data_path, seq_name, "hdVideos", hd_vid_name)
         if (not os.path.exists(hd_vid_path)):
             print(f"Path {hd_vid_path} does not exist")
+        
         cap = cv2.VideoCapture(hd_vid_path)
         if (not cap.isOpened()):
             print(f"Could not open {hd_vid_path}")
         
+        
+        hd_idx = 0
+        while True:
+            skel_json_fname = hd_skel_json_path+'body3DScene_{0:08d}.json'.format(hd_idx)
+            with open(skel_json_fname) as dfile:
+                bframe = json.load(dfile)
+
+            for body in bframe['bodies']:
+                skel = np.array(body['joints19']).reshape((-1,4)).transpose()
+
+            joint_world = skel[0:3]
+            joints_cam = (np.dot(cam['R'], joint_world.T) + cam['t']).T
+            print(joints_cam)
+            input("? ")
+            hd_idx += 1
 
 def process(sequences):
     for sequence in sequences:
